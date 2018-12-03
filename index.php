e5eaff90680205dfe3f950d7fb2484c6b269ef60
KawasakiHachi <kd1221092@st.kobedenshi.ac.jp>
2018-12-03 17:01:19 +0900

    header調整　画像ファイル読み込み

--- index.php
+++ index.php
@@ -2,3 +2,12 @@
 	$page = "トップ";
 	require_once __DIR__ . '/bootstrap/header.html';
  ?>
+
+ <div class="input-group">
+    <label class="input-group-btn">
+        <span class="btn btn-primary">
+            画像を選択<input type="file" style="display:none">
+        </span>
+    </label>
+    <input type="text" class="form-control" readonly="">
+</div>
